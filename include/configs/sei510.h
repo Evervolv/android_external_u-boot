9cad45b25bcb5947e54ea59e58670c23c60b5495
Nick Reuter <nreuter85@gmail.com>
2024-11-29 22:51:21 -0600

    meson64_common: commonize partition layout
     Device can override to suit their needs.
    
    Change-Id: I9bc14bd2612344850a76faff7e30326d4aaabf6a

--- include/configs/sei510.h
+++ include/configs/sei510.h
@@ -9,9 +9,6 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
-#define LOGO_UUID "43a3305d-150f-4cc9-bd3b-38fca8693846;"
-#define ROOT_UUID "ddb8c3f6-d94d-4394-b633-3134139cc2e0;"
-
 #define PARTS_DEFAULT                                        \
 	"uuid_disk=${uuid_gpt_disk};"  	\
 	"name=logo,size=2M,uuid=" LOGO_UUID             \
@@ -24,7 +21,6 @@
 	"name=super,size=2304M,uuid=${uuid_gpt_super};"	\
 	"name=userdata,size=4820M,uuid=${uuid_gpt_userdata};" \
 	"name=rootfs,size=-,uuid=" ROOT_UUID
-
 
 #include <configs/meson64_android.h>
 
